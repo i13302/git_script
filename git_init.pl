f0fe345fe38ed500e2fbf85e50540f816c022595
Hukanzen <qazxswed1934@gmail.com>
2016-03-28 22:19:24 +0900

    1st Commit

---
+++ git_init.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+#$init_Lpath"";
+#chdir $init_Lpath;
+
+$remote_path=$ARGV[0];
+
+$USER="Hukanzen";
+$PASS="6268ad596406d28b3558144edd559d2a5c027d23";
+
+#=== git init ===#
+$git_init="git init";
+#=== git init ===#
+
+#=== git remote add ===#
+$github_prev="://github";
+$github_next=sprintf("://%s\:%s\@github",$USER,$PASS);
+$remote_path=~s!$github_prev!$github_next!;
+$dir_name=`pwd`;
+$dir_name=~s!^.*/!!;
+chomp($dir_name);
+
+$git_remote_add=sprintf("git remote add %s %s",$dir_name,$remote_path);
+#=== git remote add ===#
+
+#=== git add * ===#
+$git_add="git add *";
+#=== git add * ===#
+
+#=== git commit ===#
+print "WHAT SENTENCE ?:";
+$cmmt=<STDIN>;
+chomp($cmmt);
+$git_commit="git commit -m \"$cmmt\"";
+#=== git commit ===#
+
+#=== git push ===#
+$git_push=sprintf("git push %s master",$dir_name);
+#=== git push ===#
+
+#=== Execute ===#
+system($git_init);
+system($git_remote_add);
+system($git_add);
+system($git_commit);
+system($git_push);
---
+++ git_push.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/perl
+
+$github_path="/home/t552/github/";
+
+#=== exclusion ===#
+@exclusion=(
+	["html","./tweet/main/user.php"],
+	["html","./streaming/main/user.php"],
+#	["myscript","./rename_*.sh"],
+);
+#=== exclusion ===#
+
+chdir $github_path;
+@file=glob "*";
+
+foreach my $my_file (@file){
+	if($my_file=~m/.+\..+/){
+		next;	
+	}elsif($my_file=~m/\./){
+		next;
+	}elsif($my_file=~m/\.\./){
+		next;
+	}
+
+	chdir $my_file;	
+
+
+	#=== watch commit ===#
+	$git_log=`git log`;
+	my @r_git_log=split(/\n/,$git_log);
+
+	print $my_file;
+	print @r_git_log[4]."\n";
+
+	print "Do you exchange ? [YES/END] :";
+	$change=<STDIN>;
+	chomp($change);
+
+	if($change eq "YES"){
+
+		#===git add all file ===#
+		system("git add *");
+
+		for(my $i=0;$exclusion[$i][0];$i++){
+			if($my_file eq $exclusion[$i][0]){
+				$s=sprintf("git rm --cached %s",$exclusion[$i][1]);
+				#print $s;
+				system($s);
+			}
+		}
+
+	
+		#=== new commit ===#	
+		print "What sentence commit ? : ";
+		$commit_sentence=<STDIN>;
+		chomp($commit_sentence);
+	
+		$git_commit="git commit -m \'".$commit_sentence."\'";
+		print $git_commit."\n";
+		system($git_commit);
+		
+		#=== push ===#
+		$git_push="git push ".$my_file." master";
+		print $git_push."\n";
+		system($git_push);
+		
+		print "\n";
+	}elsif($change eq "END"){
+		last;	
+	}
+	
+	chdir "../";
+
+}
+
+print "END\n";
